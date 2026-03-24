22ed83cf1963c1cbf7e55892b4f65e2d8ec064ba
Michael McNamara <m1k3.mcnamara@outlook.com>
2026-03-24 18:22:10 +0200

    Enable CORS for Flutter app integration
    Added a CORS policy "AllowFlutterApp" to permit any origin, header, and method, allowing external clients such as Flutter apps to access the API. Registered the policy in the service container and applied it in the middleware pipeline before authorization.

--- FollowMeFree.API/Program.cs
+++ FollowMeFree.API/Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFlutterApp", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,6 +34,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFlutterApp");
 
 app.UseAuthorization();
 
