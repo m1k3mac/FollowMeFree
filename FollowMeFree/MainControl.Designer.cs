301fddea101a4b265806d9773d36e0d6118fda8b
Michael McNamara <m1k3.mcnamara@outlook.com>
2026-03-26 15:18:21 +0200

    populated printjobs grid

--- FollowMeFree/MainControl.Designer.cs
+++ FollowMeFree/MainControl.Designer.cs
@@ -28,24 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
-            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.gaugeControl1 = new DevExpress.XtraGauges.Win.GaugeControl();
-            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
             this.digitalGauge3 = new DevExpress.XtraGauges.Win.Gauges.Digital.DigitalGauge();
             this.digitalBackgroundLayerComponent1 = new DevExpress.XtraGauges.Win.Gauges.Digital.DigitalBackgroundLayerComponent();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.printJobBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colUserName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDocumentName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colJobId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colPages = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDateTimePrinted = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDataType = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDepartment = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.digitalGauge3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.digitalBackgroundLayerComponent1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.printJobBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
@@ -60,41 +70,6 @@
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
-            // Root
-            // 
-            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
-            this.Root.GroupBordersVisible = false;
-            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem1,
-            this.layoutControlItem2});
-            this.Root.Name = "Root";
-            this.Root.Size = new System.Drawing.Size(841, 577);
-            this.Root.TextVisible = false;
-            // 
-            // gridControl1
-            // 
-            this.gridControl1.Location = new System.Drawing.Point(221, 12);
-            this.gridControl1.MainView = this.gridView1;
-            this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(608, 553);
-            this.gridControl1.TabIndex = 4;
-            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.gridView1});
-            // 
-            // gridView1
-            // 
-            this.gridView1.GridControl = this.gridControl1;
-            this.gridView1.Name = "gridView1";
-            // 
-            // layoutControlItem1
-            // 
-            this.layoutControlItem1.Control = this.gridControl1;
-            this.layoutControlItem1.Location = new System.Drawing.Point(209, 0);
-            this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(612, 557);
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem1.TextVisible = false;
-            // 
             // gaugeControl1
             // 
             this.gaugeControl1.Gauges.AddRange(new DevExpress.XtraGauges.Base.IGauge[] {
@@ -103,14 +78,6 @@
             this.gaugeControl1.Name = "gaugeControl1";
             this.gaugeControl1.Size = new System.Drawing.Size(100, 553);
             this.gaugeControl1.TabIndex = 5;
-            // 
-            // layoutControlItem2
-            // 
-            this.layoutControlItem2.Control = this.gaugeControl1;
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(209, 557);
-            this.layoutControlItem2.TextSize = new System.Drawing.Size(93, 13);
             // 
             // digitalGauge3
             // 
@@ -132,6 +99,115 @@
             this.digitalBackgroundLayerComponent1.TopLeft = new DevExpress.XtraGauges.Core.Base.PointF2D(26F, 0F);
             this.digitalBackgroundLayerComponent1.ZOrder = 1000;
             // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.printJobBindingSource;
+            this.gridControl1.Location = new System.Drawing.Point(221, 12);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(608, 553);
+            this.gridControl1.TabIndex = 4;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // printJobBindingSource
+            // 
+            this.printJobBindingSource.DataSource = typeof(FollowMeFree.PrintJob);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Appearance.EvenRow.BackColor = System.Drawing.Color.MintCream;
+            this.gridView1.Appearance.EvenRow.Options.UseBackColor = true;
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colUserName,
+            this.colDocumentName,
+            this.colJobId,
+            this.colPages,
+            this.colDateTimePrinted,
+            this.colDataType,
+            this.colDepartment});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsFind.AlwaysVisible = true;
+            this.gridView1.OptionsView.EnableAppearanceEvenRow = true;
+            // 
+            // colUserName
+            // 
+            this.colUserName.FieldName = "UserName";
+            this.colUserName.Name = "colUserName";
+            this.colUserName.Visible = true;
+            this.colUserName.VisibleIndex = 0;
+            // 
+            // colDocumentName
+            // 
+            this.colDocumentName.FieldName = "DocumentName";
+            this.colDocumentName.Name = "colDocumentName";
+            this.colDocumentName.Visible = true;
+            this.colDocumentName.VisibleIndex = 1;
+            // 
+            // colJobId
+            // 
+            this.colJobId.FieldName = "JobId";
+            this.colJobId.Name = "colJobId";
+            this.colJobId.Visible = true;
+            this.colJobId.VisibleIndex = 2;
+            // 
+            // colPages
+            // 
+            this.colPages.FieldName = "Pages";
+            this.colPages.Name = "colPages";
+            this.colPages.Visible = true;
+            this.colPages.VisibleIndex = 3;
+            // 
+            // colDateTimePrinted
+            // 
+            this.colDateTimePrinted.FieldName = "DateTimePrinted";
+            this.colDateTimePrinted.Name = "colDateTimePrinted";
+            this.colDateTimePrinted.Visible = true;
+            this.colDateTimePrinted.VisibleIndex = 4;
+            // 
+            // colDataType
+            // 
+            this.colDataType.FieldName = "DataType";
+            this.colDataType.Name = "colDataType";
+            this.colDataType.Visible = true;
+            this.colDataType.VisibleIndex = 5;
+            // 
+            // colDepartment
+            // 
+            this.colDepartment.FieldName = "DocumentName";
+            this.colDepartment.Name = "colDepartment";
+            this.colDepartment.Visible = true;
+            this.colDepartment.VisibleIndex = 6;
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(841, 577);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.gridControl1;
+            this.layoutControlItem1.Location = new System.Drawing.Point(209, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(612, 557);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.gaugeControl1;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(209, 557);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(93, 13);
+            // 
             // MainControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -139,15 +215,17 @@
             this.Controls.Add(this.layoutControl1);
             this.Name = "MainControl";
             this.Size = new System.Drawing.Size(841, 577);
+            this.Load += new System.EventHandler(this.MainControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.digitalGauge3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.digitalBackgroundLayerComponent1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.printJobBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -163,5 +241,13 @@
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private System.Windows.Forms.BindingSource printJobBindingSource;
+        private DevExpress.XtraGrid.Columns.GridColumn colUserName;
+        private DevExpress.XtraGrid.Columns.GridColumn colDocumentName;
+        private DevExpress.XtraGrid.Columns.GridColumn colJobId;
+        private DevExpress.XtraGrid.Columns.GridColumn colPages;
+        private DevExpress.XtraGrid.Columns.GridColumn colDateTimePrinted;
+        private DevExpress.XtraGrid.Columns.GridColumn colDataType;
+        private DevExpress.XtraGrid.Columns.GridColumn colDepartment;
     }
 }
--- FollowMeFree/MainControl.cs
+++ FollowMeFree/MainControl.cs
@@ -13,9 +13,16 @@ namespace FollowMeFree
 {
     public partial class MainControl : DevExpress.XtraEditors.XtraUserControl
     {
+        private FMFDataEntities _dbContext = new FMFDataEntities();
         public MainControl()
         {
             InitializeComponent();
+        }
+
+        private void MainControl_Load(object sender, EventArgs e)
+        {
+            var query = _dbContext.PrintJobs.OrderByDescending(p => p.DateTimePrinted).Take(500).ToList();
+            printJobBindingSource.DataSource = query;
         }
     }
 }
